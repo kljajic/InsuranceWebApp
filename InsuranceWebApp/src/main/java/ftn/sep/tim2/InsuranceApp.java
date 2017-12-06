2fb21966b2a5f650b8777e5610b7d80cb6d9e334
kljajic <kljajic77@gmail.com>
2017-12-06 18:49:57 +0100

    Initial commit.

---
+++ InsuranceWebApp/src/main/java/ftn/sep/tim2/InsuranceApp.java
@@ -0,0 +1,13 @@
+package ftn.sep.tim2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceApp {
+    
+	public static void main( String[] args ){
+		SpringApplication.run(InsuranceApp.class, args);
+    }
+	
+}
---
+++ InsuranceWebApp/src/test/java/ftn/sep/tim2/InsuranceAppTest.java
@@ -0,0 +1,38 @@
+package ftn.sep.tim2;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class InsuranceAppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public InsuranceAppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( InsuranceAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
