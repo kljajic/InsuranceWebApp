7a8fc86397725aa42c6fbaa55b71549755124010
kljajic <kljajic77@gmail.com>
2017-12-11 17:28:56 +0100

    Added NoArgConstractors, getters and setters for model.

--- InsuranceWebApp/src/main/java/ftn/sep/tim2/model/KontekstAtributa.java
+++ InsuranceWebApp/src/main/java/ftn/sep/tim2/model/KontekstAtributa.java
@@ -1,11 +1,44 @@
 package ftn.sep.tim2.model;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-public class KontekstAtributa {
+public class KontekstAtributa implements Serializable{
 
+	private static final long serialVersionUID = 2169394029110006240L;
 	private Long id;
 	private String naziv;
-	private List<TipAtributa> tipoviatributa;
+	private List<TipAtributa> tipoviAtributa;
+	
+	public KontekstAtributa() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public List<TipAtributa> getTipoviAtributa() {
+		if(tipoviAtributa == null) {
+			tipoviAtributa = new ArrayList<>();
+		}
+		return tipoviAtributa;
+	}
+
+	public void setTipoviAtributa(List<TipAtributa> tipoviAtributa) {
+		this.tipoviAtributa = tipoviAtributa;
+	}
 	
 }
